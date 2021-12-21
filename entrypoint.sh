3155fba57a9da508cc665fe635d4783e842c3bd1
Fabio Calefato <fcalefato@gmail.com>
2021-12-21 14:10:09 +0100

    Rename log files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # this script is used to boot the Docker container
 
-touch /logs/error.log
-touch /logs/access.log
+touch /logs/server.log
+touch /logs/app.log
 
 exec gunicorn main:app \
     --bind=0.0.0.0:80 \
     --log-level=info \
-    --error-logfile=/logs/error.log \
-    --access-logfile=/logs/info.log
+    --log-file=/logs/server.log \
+    --access-logfile=/logs/app.log
